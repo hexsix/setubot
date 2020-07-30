a7e094e532ad8a7674ec8707df9b094f026192ee
hexsix <hexsixology@gmail.com>
2020-07-30 11:42:24 +0800

    ref(setu): 分离setu模块

---
+++ main.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+""" --------------------------------------
+Filename: main.py
+Description: bot 主程序
+Author: hexsix
+Date: 2020/07/29
+-------------------------------------- """
+import json
+
+from mirai import Mirai, Plain, MessageChain, Friend, Image, GroupMessage, Group, Member, FriendMessage
+
+from setu import random_setu, newest_setu
+
+config = json.load(open('config.json'))
+qq = config['qq']  # 字段 qq 的值
+authKey = config['authKey']  # 字段 authKey 的值
+mirai_api_http_locate = config['port']  # httpapi所在主机的地址端口,如果 setting.yml 文件里字段 "enableWebsocket" 的值为 "true" 则需要将 "/" 换成 "/ws", 否则将接收不到消息.
+allowed_groups = config['allowedGroups']
+
+app = Mirai(f"mirai://{mirai_api_http_locate}?authKey={authKey}&qq={qq}")
+
+
+@app.receiver(GroupMessage)
+async def GMHandler(app: Mirai, group: Group, member: Member, message: GroupMessage):
+    if group.id in allowed_groups:
+        if message.toString() == '小六，涩图':
+            await app.sendGroupMessage(group, [
+                Image.fromFileSystem(random_setu()),
+            ])
+        elif message.toString() == '小六，新涩图':
+            for temp_path in newest_setu():
+                await app.sendGroupMessage(group, [
+                    Image.fromFileSystem(temp_path),
+                ])
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ setu.py
@@ -0,0 +1,103 @@
+# coding: utf-8
+
+""" --------------------------------------
+Filename: setu.py
+Description: RT
+Author: hexsix
+Date: 2020/07/29
+-------------------------------------- """
+import datetime
+import os
+import time
+from random import randint
+from typing import List, Tuple
+
+import cv2
+import numpy as np
+
+
+setu_dir = r'./imgs/hso'
+
+
+def get_sorted_setu_paths() -> List[Tuple[str, datetime.datetime]]:
+    """
+    遍历 setu 文件夹，返回按照修改时间排序的 setu，新的在前
+    :return: List[Tuple[setu_path, modified_time]]
+    """
+    ret = []
+    for path, _, file_list in os.walk(setu_dir):
+        for file_name in file_list:
+            if file_name[0] == '.':
+                continue        # ignore hidden files
+            file_path = os.path.join(path, file_name)
+            modified_time = time.localtime(os.stat(file_path).st_mtime)
+            y = time.strftime('%Y', modified_time)
+            m = time.strftime('%m', modified_time)
+            d = time.strftime('%d', modified_time)
+            H = time.strftime('%H', modified_time)
+            M = time.strftime('%M', modified_time)
+            then = datetime.datetime(int(y), int(m), int(d), int(H), int(M))
+            ret.append((file_path, then))
+    ret.sort(key=lambda item: item[1], reverse=True)
+    return ret
+
+
+def get_temp_path(img_path):
+    suffix = '.' + img_path.split('.')[-1]
+    if suffix == '.jfif':
+        suffix = '.jpg'        # twitter jfif is jpg, cv2 recgonize jpg
+    ret = r'./imgs/temp' + suffix
+    return ret
+
+
+def random_setu() -> str:
+    """
+    从 setu 文件夹中随机抽取一张 setu，撒盐，保存到 temp.jpg/temp.png
+    :return: temp.jpg/temp.png
+    """
+    setu_list = get_sorted_setu_paths()
+    idx = randint(0, len(setu_list) - 1)
+    setu_path = setu_list[idx][0]
+    setu = cv2.imread(setu_path)
+    salty_setu = salt(setu)
+    temp_path = get_temp_path(setu_path)
+    cv2.imwrite(temp_path, salty_setu)
+    return temp_path
+
+
+def newest_setu(n: int = 3) -> List[str]:
+    """
+    从 setu 文件夹中返回最新的 n 张椒盐 setu
+    :param n: RT
+    :return: RT
+    """
+    setu_paths = [item[0] for item in get_sorted_setu_paths()[:n]]
+    for setu_path in setu_paths:
+        setu = cv2.imread(setu_path)
+        salty_setu = salt(setu)
+        temp_path = get_temp_path(setu_path)
+        cv2.imwrite(temp_path, salty_setu)
+        yield temp_path
+
+
+def salt(img: np.ndarray, n: int = 6) -> np.ndarray:
+    """
+    椒盐setu
+    :param img: img mat
+    :param n: number of salt
+    :return: salty img mat
+    """
+    for k in range(n):
+        i = randint(0, img.shape[1] - 1)
+        j = randint(0, img.shape[0] - 1)
+        if img.ndim == 2:
+            img[j, i] = 255
+        elif img.ndim == 3:
+            img[j, i, 0] = randint(0, 255)
+            img[j, i, 1] = randint(0, 255)
+            img[j, i, 2] = randint(0, 255)
+    return img
+
+
+if __name__ == '__main__':
+    pass
